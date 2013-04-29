2621d1fd17e3372d77776d95ef18265f4c0d3070
thiago <thiago@thiago>
2013-04-29 17:05:56 -0300

    power grid

--- src/test/groovy/br/com/tbp/algorithm/ModularityTest.groovy
+++ src/test/groovy/br/com/tbp/algorithm/ModularityTest.groovy
@@ -1,43 +1,58 @@
 package br.com.tbp.algorithm
 
-import br.com.tbp.parser.FBJsonParser
 import br.com.tbp.support.FileReader
 import br.com.tbp.model.Graph
 import br.com.tbp.model.Node
-import br.com.tbp.printer.GMLPrinter
-import br.com.tbp.file.FileManager
+import br.com.tbp.model.Edge
 
 
 class ModularityTest extends GroovyTestCase {
 
-    private String jsonStringExpected;
-    private String jsonString;
-    private FBJsonParser fbJsonParser;
+
+    private String xmlString;
+
     private Modularity modularity;
 
     public ModularityTest() {
-        jsonString = FileReader.readFile("src/test/resources/br/com/tbp/parser/feed_cibercultura.txt")
-        fbJsonParser = new FBJsonParser()
+        xmlString = FileReader.readFile("src/test/resources/br/com/tbp/algorithm/PowerGrid.graphml")
         modularity = new Modularity()
-        jsonStringExpected = FileReader.readFile("src/test/resources/br/com/tbp/parser/fb.txt")
+
     }
 
     void testCompute() {
-        Graph graph = fbJsonParser.parse(jsonString);
-        FileManager fileManager = new FileManager();
-        GMLPrinter printer = new GMLPrinter(fileManager);
 
-        printer.print(graph)
-        modularity.setRandom(true)
-        modularity.setUseWeight(true)
+        def xml = new XmlSlurper().parseText(xmlString)
+        def nodes = xml.graph.node
+        def edges = xml.graph.edge
+
+        def nodeMap = new HashMap<String, Node>()
+
+        nodes.each { node ->
+            def n = new Node(node.@id.text(), node.@id.text())
+            nodeMap.put(n.id, n)
+        }
+
+         def edgeMap = new HashMap<String, Edge>()
+
+        edges.each { edge ->
+            def node1 = nodeMap.get(edge.@source.text())
+            def node2 = nodeMap.get(edge.@target.text())
+            def e = new Edge(node1, node2)
+            e.setWeight(1F)
+            edgeMap.put(e.id, e)
+        }
+
+        Graph graph = new Graph();
+        graph.getEdgeSet().addAll(edgeMap.values())
+        graph.getNodeSet().addAll(nodeMap.values())
+
+        modularity.setRandom(false)
+        modularity.setUseWeight(false)
         modularity.execute(graph)
 
         println(graph.getModularity())
         println(graph.getModularityResolution())
         println(graph.getNumberOfCommunities())
 
-        for (Node node: graph.getNodeSet()) {
-            println(node.name + " " + node.modularityClass);
-        }
     }
 }
