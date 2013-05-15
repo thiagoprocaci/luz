56f527f503ae936ea5adacf2ca7bd3088afaac9a
tbp <thiagoprocaci@gmail.com>
2013-05-15 01:48:57 -0300

    remocao variavel desnecessaria

--- src/main/groovy/br/com/tbp/file/FileManager.groovy
+++ src/main/groovy/br/com/tbp/file/FileManager.groovy
@@ -4,7 +4,6 @@ package br.com.tbp.file
 class FileManager {
 
     def save(String text) {
-        def date = new Date()
         def file = new File("files" +  File.separator + "fb.gml")
         FileOutputStream fop = new FileOutputStream(file);
         fop.write(text.getBytes());
